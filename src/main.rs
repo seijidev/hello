08edf0bc8a30d2e7903a8019697772263730ccdc
seiji <51401797+seijidev@users.noreply.github.com>
2024-04-24 19:15:41 -0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    println!("Hello, world!");
+    let rect1 = Rectangle {
+        width: 3,
+        height: 5,
+    };
+
+    println!("{:?})", rect1);
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: i32,
+    height: i32,
+}
+
+fn test(a: i32, b: i32) -> {
+    
+}
